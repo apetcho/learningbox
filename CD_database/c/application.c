8e2d5d07d212758cabb53c46d926d92e60a243a6
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-03-20 16:38:55 +0100

    Add function prototypes

--- CD_database/c/application.c
+++ CD_database/c/application.c
@@ -26,3 +26,23 @@ static char current_cat[MAX_STRING];
 const char *title_file = "title.cdb";
 const char *tracks_file = "tracks.cdb";
 const char *temp_file = "cdb.tmp";
+
+
+/**
+ * The function prototypes.
+ */
+
+void clear_all_screen(void);
+void get_return(void);
+int get_confirm(void);
+int get_choice(char *greet, char *choices[]);
+void draw_menu(char *options[], int highlight, int start_row, int start_col);
+void insert_title(char *cdtitle);
+void get_string(char *string);
+void add_record(void);
+void count_cds(void);
+void find_cd(void);
+void list_tracks(void);
+void remove_tracks(void);
+void remove_cd(void);
+void update_cd(void);
