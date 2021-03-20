dd9f789beb96b7df04b9af87f1ef7e91a63919f5
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-03-20 16:56:23 +0100

    implement the main() driver ahead of other functions

--- CD_database/c/application.c
+++ CD_database/c/application.c
@@ -71,3 +71,62 @@ char *extended_menu[] = {
     "quit",
     0,
 };
+
+
+/**/
+int main()
+{
+    int choice;
+    initscr();
+
+    // main loop
+    do {
+        choice = get_choice("Options:",
+                current_cd[0] ? extended_menu : main_menu);
+        switch(choice){
+        case 'q':
+            break;
+        case 'a':
+            add_record();
+            break;
+        case 'c':
+            count_cds();
+            break;
+        case 'f':
+            find_cd();
+            break;
+        case 'l':
+            list_tracks();
+            break;
+        case 'r':
+            remove_cd();
+            break;
+        case 'u':
+            update_cd();
+            break;
+        }
+    }while(choice != 'q');
+    endwin();
+
+    return EXIT_SUCCESS;  // or exit(EXIT_SUCCESS);
+}
+
+/******************************
+ * Functions' implementation  *
+ ******************************/
+void clear_all_screen(void);
+void get_return(void);
+int get_confirm(void);
+int get_choice(char *greet, char *choices[]);
+void draw_menu(char *options[], int highlight, int start_row, int start_col);
+void insert_title(char *cdtitle);
+void get_string(char *string);
+void add_record(void);
+void count_cds(void);
+void find_cd(void);
+void list_tracks(void);
+void remove_tracks(void);
+void remove_cd(void);
+void update_cd(void);
+
+
