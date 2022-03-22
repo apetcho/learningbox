5291f7ff8a64eea1c9c81a63a7a526aba47d0529
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-22 20:18:30 +0100

    Definite initial xnr_set data structure interface

---
+++ xnrooc/src/xnr_set.h
@@ -0,0 +1,12 @@
+#ifndef _XNR_SET_H
+#define _XNR_SET_H
+
+extern const void* xnr_set;
+
+void* xnr_set_add(void* set, const void* element);
+void* xnr_set_find(void* set, const void* element);
+void* xnr_set_drop(void* set, const void* element);
+int xnr_set_contains(void* set, const void* element);
+unsigned xnr_set_length(void* set, const void* element);
+
+#endif
