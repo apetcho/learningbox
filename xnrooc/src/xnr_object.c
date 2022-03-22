b03919bcbbafed60a7719e6fc2d4eb1daa9964d6
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-22 22:07:05 +0100

    Implement xnr_new()

--- xnrooc/src/xnr_object.c
+++ xnrooc/src/xnr_object.c
@@ -1,4 +1,6 @@
 #include<stdarg.h>
+#include<stdlib.h>
+#include<assert.h>
 #include<stdio.h>
 
 #include "xnr_object.h"
@@ -11,7 +13,21 @@ struct xnr_class{
 };
 
 //
-void* xnr_new(const void *klass, ...){}
+void* xnr_new(const void *_klass, ...){
+    const xnr_class *klass = _klass;
+    void *ptr = calloc(1, klass->size);
+    assert(ptr);
+    *(xnr_class **)ptr = klass;
+
+    if(klass->ctor){
+        va_list args;
+        va_start(args, _klass);
+        ptr = klass->ctor(ptr, &args);
+        va_end(args);
+    }
+
+    return ptr;
+}
 
 //
 void xnr_delete(void *item){}
