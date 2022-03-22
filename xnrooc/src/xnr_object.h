0dc1780a2e5fc8e28f5d0991f15e73f803663606
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-22 21:54:52 +0100

    Redefine xnr_object.h

--- xnrooc/src/xnr_object.h
+++ xnrooc/src/xnr_object.h
@@ -1,9 +1,13 @@
 #ifndef _XNR_OBJECT_H
 #define _XNR_OBJECT_H
 
-extern const void* xnr_object;
-void* xnr_new(const void* type, ...);
-void xnr_delete(void* xnrobj);
-int xnr_differ(const void *lhs, const void *rhs);
+#include<stddef.h>
+
+void* xnr_new(const void* klass, ...);
+void xnr_delete(void *item);
+void* xnr_clone(const void *self);
+int xnr_differ(const void *self, const void *other);
+size_t xnr_sizeof(const void *self);
+
 
 #endif
