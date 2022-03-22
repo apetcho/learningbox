7631a1aa01aa955868e83198524a162ba2e87276
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-22 14:51:16 +0100

    Implement utility functions for maze

---
+++ maze/src/maze.c
@@ -0,0 +1,167 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdarg.h>
+#include<signal.h>
+#include<errno.h>
+#include<stdio.h>
+
+#include "maze.h"
+
+#define MAZE_ROUTINE(name) maze_ ## name
+
+#define MAZE_ERROR_MESSAGE(routine, ...) \
+    do{\
+        char buf[80];\
+        sprintf(buf, "%s(): %s", (routine), strerror(errno));\
+        fprintf(stderr, "%s\n", buf);\
+        fprintf(stderr, __VA_ARGS__);\
+        fprintf(stderr, "\n");\
+    } while(0)
+
+#define MAZE_SET_ROUTINE_INFO(index) \
+    do{\
+        maze_routine_index = index;\
+        sprintf(maze_routine_name, "%s", maze_routines[index]);\
+    }while(0)
+    
+
+struct MazeCell_s{
+    int row;
+    int col;
+    int val;
+};
+
+struct Maze_s{
+    int nrow;
+    int ncol;
+    MazeCell_t *grid;
+};
+
+typedef enum {
+    MAZE_EVENT_NONE = 100,
+    MAZE_EVENT_MEMORY_ERROR,
+    MAZE_EVENT_RUNTIME_ERROR,
+    MAZE_EVENT_INVALID_DATA
+} MazeEvent_t;
+
+enum MazeRoutine_e {
+    ALLOCATE_CELL,
+    ALLOCATE_MAZE,
+    DEALLOCATE_CELL,
+    DEALLOCATE_MAZE,
+    ADD_CELL,
+    READ_FILE,
+    WRITE_FILE,
+    PRINT,
+    FIND_PATH
+};
+typedef enum MazeRoutine_e MazeRoutine_t;
+
+static char maze_routine_name[20];
+static MazeRoutine_t maze_routine_index;
+
+static char const *const maze_routines[] = {
+    [ALLOCATE_CELL] = MAZE_ROUTINE(allocate_cell),
+    [ALLOCATE_MAZE] = MAZE_ROUTINE(allocate),
+    [DEALLOCATE_CELL] = MAZE_ROUTINE(deallocate_cell),
+    [DEALLOCATE_MAZE] = MAZE_ROUTINE(deallocate),
+    [ADD_CELL] = MAZE_ROUTINE(add_cell),
+    [READ_FILE] = MAZE_ROUTINE(read_file),
+    [WRITE_FILE] = MAZE_ROUTINE(write_file),
+    [PRINT] = MAZE_ROUTINE(print),
+    [FIND_PATH] = MAZE_ROUTINE(find_path),
+    NULL
+};
+//
+static void maze_signal_handler(int maze_signum){
+    char message[80];
+    switch(maze_signum){
+    case MAZE_EVENT_NONE:
+        return;
+    case MAZE_EVENT_INVALID_DATA:
+        sprintf(message, "invalid data.");
+        MAZE_ERROR_MESSAGE(maze_routine_name, message);
+        exit(EXIT_FAILURE);
+    case MAZE_EVENT_MEMORY_ERROR:
+        sprintf(message, "memory error.");
+        MAZE_ERROR_MESSAGE(maze_routine_name, message);
+        exit(EXIT_FAILURE);
+    case MAZE_EVENT_RUNTIME_ERROR:
+        sprintf(message, "runtime error.");
+        MAZE_ERROR_MESSAGE(maze_routine_name, message);
+        exit(EXIT_FAILURE);
+    default:
+        break;
+    }
+
+    return;
+}
+
+//
+static void maze_set_routine_info(MazeRoutine_t routine){
+    switch(routine){
+    case ALLOCATE_CELL:
+        MAZE_SET_ROUTINE_INFO(ALLOCATE_CELL);
+        break;
+    case ALLOCATE_MAZE:
+        MAZE_SET_ROUTINE_INFO(ALLOCATE_MAZE);
+        break;
+    case DEALLOCATE_CELL:
+        MAZE_SET_ROUTINE_INFO(DEALLOCATE_CELL);
+        break;
+    case DEALLOCATE_MAZE:
+        MAZE_SET_ROUTINE_INFO(DEALLOCATE_MAZE);
+        break;
+    case ADD_CELL:
+        MAZE_SET_ROUTINE_INFO(ADD_CELL);
+        break;
+    case WRITE_FILE:
+        MAZE_SET_ROUTINE_INFO(WRITE_FILE);
+        break;
+    case READ_FILE:
+        MAZE_SET_ROUTINE_INFO(READ_FILE);
+    case PRINT:
+        MAZE_SET_ROUTINE_INFO(PRINT);
+        break;
+    case FIND_PATH:
+        MAZE_SET_ROUTINE_INFO(FIND_PATH);
+        break;
+    default:
+        break;
+    }
+
+    return;
+}
+
+static void maze_error(int maze_signum, MazeRoutine_t routineIndex){
+    maze_set_routine_info(routineIndex);
+    switch(maze_signum){
+    case MAZE_EVENT_NONE:
+        signal(MAZE_EVENT_NONE, maze_signal_handler);
+        break;
+    case MAZE_EVENT_INVALID_DATA:
+        signal(MAZE_EVENT_INVALID_DATA, maze_signal_handler);
+        break;
+    case MAZE_EVENT_MEMORY_ERROR:
+        signal(MAZE_EVENT_MEMORY_ERROR, maze_signal_handler);
+        break;
+    case MAZE_EVENT_RUNTIME_ERROR:
+        signal(MAZE_EVENT_RUNTIME_ERROR, maze_signal_handler);
+        break;
+    default:
+        break;
+    }
+    return;
+}
+
+//
+MazeCell_t* maze_allocate_cell(int row, int col);
+void maze_deallocate_cell(MazeCell_t *cell);
+Maze_t* maze_allocate();
+void maze_deallocate(Maze_t *maze);
+
+void maze_add_cell(Maze_t *maze, MazeCell_t const *cell);
+Maze_t* maze_readfile(char const *filename);
+void maze_writefile(char const *filename, Maze_t *maze);
+void maze_print(Maze_t const *maze);
+int maze_find_path(Maze_t const *maze, int row, int col);
--- maze/src/maze.h
+++ maze/src/maze.h
@@ -8,26 +8,18 @@ const char MAZE_PATH_WALL = '#';
 const char MAZE_PATH_OPEN = ' ';
 const char MAZE_PATH_MARK = 'X';
 
-typedef struct maze_cell_s{
-    int row;
-    int col;
-    int val;
-} maze_cell_t;
+typedef struct MazeCell_s MazeCell_t;
 
-typedef struct maze_s{
-    int nrow;
-    int ncol;
-    maze_cell_t *grid;
-} maze_t;
+typedef struct Maze_s Maze_t;
 
-maze_cell_t* maze_allocate_cell(int row, int col);
-void maze_deallocate_cell(maze_cell_t *cell);
-maze_t* maze_allocate();
-void maze_deallocate(maze_t *maze);
-void maze_add_cell(maze_t *maze, maze_cell_t const *cell);
-maze_t* maze_readfile(char const *filename);
-void make_writefile(char const *filename, maze_t *maze);
-void maze_print(maze_t const *maze);
-int maze_find_path(maze_t const *maze, int row, int col);
+MazeCell_t* maze_allocate_cell(int row, int col);
+void maze_deallocate_cell(MazeCell_t *cell);
+Maze_t* maze_allocate();
+void maze_deallocate(Maze_t *maze);
+void maze_add_cell(Maze_t *maze, MazeCell_t const *cell);
+Maze_t* maze_read_file(char const *filename);
+void maze_write_file(char const *filename, Maze_t *maze);
+void maze_print(Maze_t const *maze);
+int maze_find_path(Maze_t const *maze, int row, int col);
 
 #endif
