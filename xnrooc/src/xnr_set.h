c9ae0159cf55ffa071d0c71956ec52e1360db13c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-22 21:32:34 +0100

    Update CMakeLists.txt

--- xnrooc/src/xnr_set.h
+++ xnrooc/src/xnr_set.h
@@ -7,6 +7,6 @@ void* xnr_set_add(void* set, const void* element);
 void* xnr_set_find(const void* set, const void* element);
 void* xnr_set_drop(void* set, const void* element);
 int xnr_set_contains(const void* set, const void* element);
-unsigned xnr_set_length(void* set, const void* element);
+unsigned xnr_set_length(const void* set);
 
 #endif
