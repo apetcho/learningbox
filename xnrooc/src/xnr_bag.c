6e80eeaaad77e2567a73f77953e7bcd13e15f2d9
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-22 20:58:27 +0100

    Implement xnr_new() for bag structure

---
+++ xnrooc/src/xnr_bag.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<assert.h>
+
+#include "xnr_object.h"
+#include "xnr_set.h"
+
+typedef struct xnr_set_{unsigned count; } xnr_set_t;
+typedef struct xnr_object_ {
+    unsigned count;
+    xnr_set_t *in;
+}xnr_object_t;
+
+static const size_t _xnr_set = sizeof(xnr_set_t);
+static const size_t _xnr_object = sizeof(xnr_object_t);
+
+const void *xnr_set = &_xnr_set;
+const void *xnr_object = &_xnr_object;
+
+//
+void* xnr_new(const void* type, ...){
+    const size_t size = *(const size_t*)type;
+    void *p = calloc(1, size);
+    assert(p);
+    return p;
+}
