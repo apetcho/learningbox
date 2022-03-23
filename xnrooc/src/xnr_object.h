9e70c49bc987b57d8d5100df36865a673f23e74d
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-24 00:10:59 +0100

    Update xnr_object.h for xnrooc6

--- xnrooc/src/xnr_object.h
+++ xnrooc/src/xnr_object.h
@@ -3,16 +3,42 @@
 
 #include<stdarg.h>
 #include<stddef.h>
+#include<stdio.h>
 
-typedef struct xnr_class{
-    size_t size;
+typedef struct xnr_object_ xnr_object_t;
+typedef struct xnr_class_  xnr_class_t;
+
+struct xnr_object_{
+    const xnr_class_t *klass;   /* object description */
+};
+
+struct xnr_class_{
+    const xnr_object_t _;                       /* class description */
+    const char *name;                           /* class name */
+    const xnr_class_t *super;                   /* super class */
+    size_t size;                                /* object size */
     void* (*ctor)(void *self, va_list *args);
     void* (*dtor)(void *self);
-    void (*draw)(const void *self);
-} xnr_class;
+    int (*differ)(const void *self, const void *b);
+    int (*puto)(const void *self, FILE *fp);
+};
+
+void* xnr_super_ctor(const void *klass, void *self, va_list *argp);
+void* xnr_super_dtor(const void *klass, void *self);
+int xnr_super_differ(const void *klass, const void *self, const void *b);
+int xnr_super_puto(const void *klass, const void *self, FILE *fp);
+
+extern const void *xnr_object;
+extern const void *xnr_class;
 
 void* xnr_new(const void* klass, ...);
 void xnr_delete(void *item);
-void xnr_draw(const void *self);
+const void* xnr_classof(const void *self);
+size_t xnr_sizeof(const void *self);
+void* xnr_ctor(void *self, va_list *argp);
+int xnr_differ(const void *self, const void *b);
+int xnr_puto(const void *self, FILE *fp);
+const void* xnr_super(const void *self);
+
 
 #endif
