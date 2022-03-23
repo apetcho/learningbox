e9d45b1adf6434ea3d998ea39a8850604a121795
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-23 14:06:36 +0100

    Implement _xnr_point_ctor() for xnrooc4

--- xnrooc/src/xnr_point.c
+++ xnrooc/src/xnr_point.c
@@ -4,7 +4,12 @@
 #include "xnr_point.h"
 #include "xnr_object.h"
 
-static void* _xnr_point_ctor(void *_self, va_list *args){}
+static void* _xnr_point_ctor(void *_self, va_list *args){
+    xnr_point_t *self = _self;
+    self->x = va_arg(*args, int);
+    self->y = va_arg(*args, int);
+    return self;
+}
 
 static void _xnr_point_draw(const void *_self){}
 
