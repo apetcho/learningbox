08d9a62644fe80d0147e70f45e1bf806362137f3
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-24 00:23:10 +0100

    Update xnr_point.h for xnrooc6

--- xnrooc/src/xnr_point.h
+++ xnrooc/src/xnr_point.h
@@ -1,15 +1,30 @@
 #ifndef _XNR_POINT_H
 #define _XNR_POINT_H
 
-typedef struct /*xnr_point_*/{
-    const void *klass;
+#include "xnr_object.h"
+
+typedef struct xnr_point_ xnr_point_t;
+typedef struct xnr_point_class_ xnr_point_class_t;
+
+struct xnr_point_{
+    const xnr_object_t _;   /* xnr_point: xnr_object */
     int x;
     int y;
-} xnr_point_t;
+};
+
+struct xnr_point_class_{
+    const xnr_class_t _;    /* xnr_point_class : xnr_class */
+    void (*draw)(const void *self);
+};
 
 extern const void *xnr_point;
+extern const void *xnr_point_class;
 
-void xnr_point_move(void *point, int dx, int dy);
+void xnr_super_draw(const void *klass, const void *self);
+void xnr_draw(const void *self);
+void xnr_move(void *point, int dx, int dy);
+void xnr_init_point(void);
+
 
 #define XNR_GETX(p)     (((const xnr_point_t*)(p))->x)
 #define XNR_GETY(p)     (((const xnr_point_t*)(p))->y)
