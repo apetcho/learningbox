1c3a30aa0c61751008b94b8c3db5dd1e96480f7a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-23 14:18:21 +0100

    Define xnr_circle structure for xnrooc4

---
+++ xnrooc/src/xnr_circle.h
@@ -0,0 +1,12 @@
+#ifndef _XNR_CIRCLE_H
+#define _XNR_CIRCLE_H
+
+#include "xnr_point.h"
+
+typedef struct xnr_circle_{
+    const xnr_point_t _;
+    int rad;
+}xnr_circle_t;
+extern const void *xnr_circle;
+
+#endif
