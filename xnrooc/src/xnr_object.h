50d7ec21a8273c02d543ef8d0696b1600e046b6f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-23 13:53:33 +0100

    Define xnr_class structure for xnrooc4

--- xnrooc/src/xnr_object.h
+++ xnrooc/src/xnr_object.h
@@ -7,16 +7,11 @@ typedef struct xnr_class{
     size_t size;
     void* (*ctor)(void *self, va_list *args);
     void* (*dtor)(void *self);
-    void* (*clone)(const void *self);
-    int (*differ)(const void *self, const void *other);
+    void (*draw)(const void *self);
 } xnr_class;
 
 void* xnr_new(const void* klass, ...);
 void xnr_delete(void *item);
-void* xnr_clone(const void *self);
-int xnr_differ(const void *self, const void *other);
-size_t xnr_sizeof(const void *self);
-size_t xnr_len(const void *self);
-
+void xnr_draw(const void *self);
 
 #endif
