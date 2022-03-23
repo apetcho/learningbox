61fa8c7c91f3556100d201239cad5e7cabe7b83b
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-23 15:24:27 +0100

    Define xnr_name implementation skeleton for xnrooc5

---
+++ xnrooc/src/xnr_name.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<stdarg.h>
+#include<string.h>
+#include<assert.h>
+
+#include "xnr_bsearch.h"
+#include "xnr_parser.h"
+#include "xnr_value.h"
+#include "xnr_name.h"
+#include "xnr_variable.h"
+
+#ifndef XNR_NAMES   /* tunable: initial size of symbol table */
+#define XNR_NAMES   4
+#endif
+
+static int _xnr_cmp(const void *_key, const void *_elt){}
+
+static xnr_name_t **_xnr_search(const char **name){}
+
+
+void xnr_install(const void *np){}
+
+void *xnr_symbol;
+
+int xnr_screen(const char *name){}
+
