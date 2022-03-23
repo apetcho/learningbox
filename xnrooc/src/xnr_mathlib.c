22d540a2d040f9101416d567d8c9426168fa4a0a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-23 17:15:59 +0100

    Implement xnr_ini__math() for xnrooc5

--- xnrooc/src/xnr_mathlib.c
+++ xnrooc/src/xnr_mathlib.c
@@ -35,3 +35,33 @@ static void _free_math(void *tree){
     xnr_delete(XNR_GETRIGHT(tree));
     free(tree);
 }
+
+static xnr_type_t _Math = {xnr_make_binary_op, _do_math, _free_math};
+const void *xnr_math = &_Math;
+
+void xnr_init_math(void){
+    static const xnr_math_t functions[] = {
+        {{ &_Math, "sin",   XNRTOK_MATH },  sin },
+		{{ &_Math, "cos",   XNRTOK_MATH },  cos },
+		{{ &_Math, "tan",   XNRTOK_MATH },  tan },
+		{{ &_Math, "asin",  XNRTOK_MATH },  asin },
+		{{ &_Math, "acos",  XNRTOK_MATH },  acos },
+		{{ &_Math, "atan",  XNRTOK_MATH },  atan },
+		{{ &_Math, "sinh",  XNRTOK_MATH },  sinh },
+		{{ &_Math, "cosh",  XNRTOK_MATH },  cosh },
+		{{ &_Math, "tanh",  XNRTOK_MATH },  tanh },
+		{{ &_Math, "exp",   XNRTOK_MATH },  exp },
+		{{ &_Math, "log",   XNRTOK_MATH },  log },
+		{{ &_Math, "log10", XNRTOK_MATH },  log10 },
+		{{ &_Math, "sqrt",  XNRTOK_MATH },  sqrt },
+		{{ &_Math, "ceil",  XNRTOK_MATH },  ceil },
+		{{ &_Math, "floor", XNRTOK_MATH },  floor },
+		{{ &_Math, "abs",   XNRTOK_MATH },  fabs },
+        {{0}}
+    };
+
+    const xnr_math_t *mp;
+    for(mp=functions; mp->_.name; ++mp){
+        xnr_install(mp);
+    }
+}
