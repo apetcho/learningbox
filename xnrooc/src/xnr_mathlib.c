6574fc44cbf092c8d40ec7a4f52d6bc8f17559a3
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-23 17:05:12 +0100

    Define XNR_FUNCTION macro for xnrooc5

---
+++ xnrooc/src/xnr_mathlib.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<assert.h>
+#include<errno.h>
+
+#include "xnr_name.h"
+#include "xnr_mathlib.h"
+#include "xnr_value.h"
+#include "xnr_parser.h"
+
+typedef struct xnr_math_{
+    xnr_name_t _;
+    double (*fun)(double);
+}xnr_math_t;
+
+#define XNR_FUNCTION(tree) (((xnr_math_t*)XNR_GETLEFT(tree))->fun)
+
+
