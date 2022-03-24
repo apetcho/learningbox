11bfeeccc6e75247e48737811cc3eda5637a5395
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-24 01:49:58 +0100

    Implement _xnr_point_ctor() for xnrooc6

--- xnrooc/src/xnr_point.c
+++ xnrooc/src/xnr_point.c
@@ -1,32 +1,36 @@
-#include<stdio.h>
-#include<stdarg.h>
+#include<assert.h>
 
 #include "xnr_point.h"
-#include "xnr_object.h"
 
-static void* _xnr_point_ctor(void *_self, va_list *args){
-    xnr_point_t *self = _self;
-    self->x = va_arg(*args, int);
-    self->y = va_arg(*args, int);
+//!@todo
+static void* _xnr_point_ctor(void *_self, va_list *argp){
+    xnr_point_t *self = xnr_super_ctor(xnr_point, _self, argp);
+    self->x = va_arg(*argp, int);
+    self->y = va_arg(*argp, int);
     return self;
 }
 
+//!@todo
 static void _xnr_point_draw(const void *_self){
     const xnr_point_t *self = _self;
     printf("<point x=%d, y=%d>\n", self->x, self->y);
 }
 
-static const xnr_class _Point = {
-    sizeof(xnr_point_t),
-    _xnr_point_ctor,
-    0,
-    _xnr_point_draw
-};
+//!@todo
+void xnr_draw(const void *_self){}
 
-const void *xnr_point = &_Point;
+//!@todo
+void xnr_super_draw(const void *klass, const void *self){}
 
-void xnr_point_move(void *_self, int dx, int dy){
-    xnr_point_t *self = _self;
-    self->x += dx;
-    self->y += dy;
-}
+//!@todo
+void xnr_move(void *_self, int dx, int dy){}
+
+//!@todo
+static void* _xnr_point_class_ctor(void *_self, va_list *argp){}
+
+// -- initialization --
+const void *xnr_point_class;
+const void *xnt_point;
+
+//!@todo
+void xnr_init_point(void){}
