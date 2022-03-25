cc22bb6ce3b6e5a08098860f3c7704b5a6d5cfe2
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-25 16:19:12 +0100

    Implement xnr_write_records() of xnrplayer for xnrcomplete

--- xnrcprogs/src/xnrplayer.c
+++ xnrcprogs/src/xnrplayer.c
@@ -48,8 +48,20 @@ int xnr_read_records(FILE *fp, int num, Player_t *them){
     return i;
 }
 
+/** Write 'num' player records to the file 'fp' from the array 'them' */
+int xnr_write_records(FILE *fp, Player_t *them, int num){
+    int i = 0;
+    fseek(fp, 0L, SEEK_SET);
+    for(i=0; i < num; i++){
+        if(fprintf(
+            fp, wrFmt, (them[i]).name, (them[i]).rank,
+            (them[i]).wins, (them[i]).losses,
+            (them[i]).last_game) < 0){ break; /* error on fprintf */}
+    }
 
-//int xnr_write_records(FILE *, Player_t *, int);
+    return i;
+}
+
 //Player_t *xnr_find_by_name(char *, Player_t *, int);
 //Player_t *xnr_find_by_rank(int, Player_t *, int);
 //void xnr_push_down(Player_t *, int, int, int);
