51fb45f5aa71f43232ca807acdf0a6198ca07751
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-25 15:34:39 +0100

    Define xnrplayer interface of xnrladder for xnrcomplete

---
+++ xnrcprogs/src/xnrplayer.h
@@ -0,0 +1,45 @@
+#ifndef _XNRPLAYER_H
+#define _XNRPLAYER_H
+/** Definitions for functions prototype which manipulate player records */
+
+#include<stddef.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include "xnroptions.h"
+
+#define NAMELEN             12      // max. for player name length
+#define LENBUF              256     // max. for input buffer length
+/** number of higher ranked players who may be challenged to move up in rank */
+#define CHALLENGE_RANGE     3 
+
+char *xnrOptArg;
+
+typedef struct {
+    char name[NAMELEN+1];
+    int rank;
+    int wins;
+    int losses;
+    time_t last_game;
+} Player_t;
+
+#define NULLPLAYER (Player_t *)0
+
+extern const char *ladder_file;
+extern const char *writeFmt;    /* used when writing records */
+extern const char *readFmt;     /* used when reading records */
+
+/** Function prototypes */
+int xnr_valid_records(FILE *);
+int xnr_read_records(FILE *, int, Player_t *);
+int xnr_write_records(FILE *, Player_t *, int);
+Player_t *xnr_find_by_name(char *, Player_t *, int);
+Player_t *xnr_find_by_rank(int, Player_t *, int);
+void xnr_push_down(Player_t *, int, int, int);
+int xnr_print_records(Player_t *, int);
+void xnr_copy_player(Player_t *, Player_t*);
+int xnr_compare_name(Player_t *, Player_t*);
+int xnr_compare_rank(Player_t *, Player_t*);
+void xnr_sort_players(Player_t *, int);
+
+#endif
