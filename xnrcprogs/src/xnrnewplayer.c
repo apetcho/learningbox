6c35dca0851a0bb18fa9afe618819875fe043c64
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-25 18:43:27 +0100

    Implement xnr_record() of xnrnewplayer for xnrcomplete

--- xnrcprogs/src/xnrnewplayer.c
+++ xnrcprogs/src/xnrnewplayer.c
@@ -10,3 +10,85 @@ static char *otherFile;
 #endif
 #define PROGNAME    "xnrnewplayer"
 static const char *usage = "usage:\n\t" PROGNAME " -r rank - name [-f file]\n"; 
+
+/** record a player */
+static void xnr_record(Player_t *player);
+
+static void _print_error(const char *message){
+    fprintf(stderr, "%s: %s\n", PROGNAME, message);
+}
+
+// ---------------------------
+//    M A I N   D R I V E R
+// ---------------------------
+int main(int argc, char **argv){}
+
+// ---
+static void xnr_record(Player_t *player){
+    int num;
+    int new_num;
+    int i;
+    Player_t *playerslist;
+    const char *fname = (otherFile == 0) ? ladderFile : otherFile;
+    FILE *fp;
+
+    fp = fopen(fname, "r+");
+    if(fp == NULL){
+        if((fp = fopen(fname, "w")) == NULL){
+            perror(PROGNAME);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    num = xnr_valid_records(fp);
+    new_num = num + 1;
+    if((player->rank <= 0) || (player->rank > new_num)){
+        char buf[80];
+        sprintf(buf, "rank must be between 1 and %d\n", new_num);
+        _print_error(buf);
+        exit(EXIT_FAILURE);
+    }
+
+    playerslist = (Player_t*)malloc(sizeof(Player_t)*new_num);
+
+    if(playerslist == NULL){
+        _print_error("out of memory");
+        exit(EXIT_FAILURE);
+    }
+
+    if(xnr_read_records(fp, num, playerslist) != num){
+        _print_error("error while reading player records");
+        free(playerslist);
+        exit(EXIT_FAILURE);
+    }
+
+    if(xnr_find_by_name(player->name, playerslist, num) != NULLPLAYER){
+        char buf[80];
+        sprintf(buf, "%s is already on the ladder", player->name);
+        _print_error(buf);
+        free(playerslist);
+        exit(EXIT_FAILURE);
+    }
+
+    xnr_copy_player(&playerslist[num], player);
+    if(player->rank != new_num){
+        xnr_push_down(playerslist, num, player->rank, num);
+    }
+
+    xnr_sort_players(playerslist, new_num);
+    if((fp = freopen(fname, "w+", fp)) == NULL){
+        perror(PROGNAME);
+        free(playerslist);
+        exit(EXIT_FAILURE);
+    }
+
+    if(xnr_write_records(fp, playerslist, new_num) != new_num){
+        _print_error("error while writing player records");
+        fclose(fp);
+        free(playerslist);
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(fp);
+    free(playerslist);
+}
