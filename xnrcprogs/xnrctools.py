d37de63baaab1f7873ecdf1df18402cfc4a6b8a0
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-25 15:02:18 +0100

    Implement compile_src() of xnrctools.py for xnrcomplete

--- xnrcprogs/xnrctools.py
+++ xnrcprogs/xnrctools.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 import shutil
 import subprocess
 import os
-from typing import List
+from typing import List, Dict
 
 
 XNRSRCDIR = os.path.join(os.path.dirname(__file__), "src")
@@ -12,7 +12,7 @@ XNRLIBDIR = os.path.join(os.path.dirname(__file__), "lib")
 XNRBINDIR = os.path.join(os.path.dirname(__file__), "bin")
 XNRBUILDIR = os.path.join(os.path.dirname(__file__), "build")
 
-XNRCFLAG = "-c -g -O2"
+XNRCFLAG = "-c -g -O2".split()
 XNRCLIBS = None
 
 XNRData = namedtuple("XNRData", "name src bin lib cflags clibs")
@@ -28,7 +28,7 @@ def xnr_get_compiler() -> None:
 
     if _cc is None:
         raise ValueError("No C compiler found on this system")
-    print(f" -- C compiler: {_cc}")
+    # print(f" -- C compiler: {_cc}")
     return _cc
 
 
@@ -46,38 +46,82 @@ class XNRCTools:
     def __init__(self, arg: XNRData) -> None:
         """Initialize XNRCTools"""
         self._name: str = arg.name
-        self._src: List[str] = arg.src
+        self._src: Dict[str, List[str]] = arg.src
         self._bin: str = arg.bin
         self._lib: List[str] = arg.lib
         self._cflags: List[str] = arg.cflags
         self._clibs: List[str] = arg.clibs
 
-    def _compile_src(self, arg: List[str]) -> None:
+    def _make_executable(self, cc, ofiles: List[str]):
+        """make executable"""
+        cmd = [cc, "-o", self._name]
+        cmd.extend(ofiles)
+        _ = subprocess.run(cmd)
+
+    def compile_src(self) -> None:
         """Compiler source"""
         here = os.getcwd()
+        print(f"directory: {here}")
+        
+        sources = self._src
+        data = {}
+        for file, headers in sources.items():
+            if os.path.isfile(file):
+                key = os.path.basename(file)
+                print(f"Found source file: {file} with basename:: {key}")
+                shutil.copy(file, XNRBUILDIR)
+                data[os.path.basename(file)] = list()
+                for header in headers:
+                    if os.path.isfile(header):
+                        hdr = os.path.basename(header)
+                        print(f"\tHeader file: {header} with basename:: {hdr}")
+                        shutil.copy(header, XNRBUILDIR)
+                        data[key].append(hdr)
+        # copy src files to build-dir
+        # copy header files to build-dir
+        print(f"{data!r}")
         os.chdir(XNRBUILDIR)
-        _ = subprocess.run(arg)
+        cc = xnr_get_compiler()
+        print(f" -- C compiler: {cc}")
+        cmd = [cc]
+        cmd.extend(XNRCFLAG)
+        for key in data:
+            _extra = [key]
+            for h in data[key]:
+                _extra.append(h)
+            cmd.extend(_extra)
+            txt = " ".join(cmd)
+            print(f"Running command:\n  {txt}")
+            subprocess.run(cmd)
+
+        ofiles = [f"{os.path.splitext(file)[0]}.o" for file in data]
+        print(ofiles)
+        self._make_executable(cc, ofiles)
+
         os.chdir(here)
+
+        
 
     def _build_bin(self, arg):
         pass
 
-    def _get_compiler(self) -> None:
-        """Get compiler"""
-        self._cc = shutil.which("clang")
-        if self._cc is None:
-            for cc in ("clang", "gcc"):
-                self._cc = shutil.which(cc)
-                if self._cc is not None:
-                    break
-
-        if self._cc is None:
-            raise ValueError("No C compiler found on this system")
-        print(f" -- C compiler: {self._cc}")
 
 
 def main():
-    _ = xnr_get_compiler()
+    #_ = xnr_get_compiler()
+    xnrgrepdata = XNRData(
+        name="xnrgrep",
+        src={
+            "src/xnroptions.c": ["src/xnroptions.h"],
+            "src/xnrgrep.c": ["src/xnroptions.h"]
+        },
+        bin="xnrgrep",
+        lib=None,
+        cflags=None,
+        clibs=None,
+    )
+    xnrgrep = XNRCTools(xnrgrepdata)
+    xnrgrep.compile_src()
 
 
 if __name__ == "__main__":
