d50f4d725bc4635438a24a8eed7cb75f4383fa9f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-26 13:40:13 +0100

    Implement vector_malloc() of xvector for Bank App

--- bank/src/xvector.c
+++ bank/src/xvector.c
@@ -18,3 +18,21 @@ struct XVector_{
     ItemDeallocator deallocate;
 };
 
+/** Initialize a vector with a specified capacity and itemsize */
+XVector_t* vector_malloc(
+    ItemAllocator allocate, ItemDeallocator deallocate,
+    size_t capacity, size_t itemsize
+){
+    XVector_t *vec = (XVector_t*)malloc(sizeof(*vec));
+    if(vec){
+        size_t cap = (capacity == 1) ? DEFAULT_CAPACITY : capacity;
+        vec->data = allocate(cap * itemsize);
+        vec->capacity = (vec->data == 0)? 0: cap;
+        vec->size = 0;
+        vec->itemsize = itemsize;
+        vec->allocate = allocate;
+        vec->deallocate = deallocate;
+    }
+
+    return vec;
+}
