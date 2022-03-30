eec3fdb2d7b5dfceba09124eeeca8db4e3cfa003
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-30 10:41:21 +0200

    Experiment with pipe & fork system call combination

---
+++ ipctuto/src/pipeExple2.c
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdio.h>
+
+
+#define MSGSIZE 32
+
+
+char *msg1 = "Guten Tag! #1";
+char *msg2 = "Hello World! #2";
+char *msg3 = "Bonjour le monde! #3";
+
+
+int main(){
+    char inbuf[MSGSIZE];
+    int p[2], i=0, rsize=0;
+    pid_t pid;
+
+    if(pipe(p) == -1){
+        perror("pipe call");
+        exit(EXIT_FAILURE);
+    }
+
+    switch(pid=fork()){
+    case -1:
+        perror("fork call");
+        exit(2);
+    case 0: // child process
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+        break;
+    default: // parent process
+        for(i=0; i < 3; i++){
+            rsize = read(p[0], inbuf, MSGSIZE);
+            printf("%.*s", rsize, inbuf);
+        }
+        wait(NULL);
+    }
+
+    return EXIT_SUCCESS;
+}
