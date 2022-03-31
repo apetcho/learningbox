0373504f505fede512a9db85207dac888d9af9bb
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-31 23:16:59 +0200

    Update common interface

--- sockettuto/src/common.h
+++ sockettuto/src/common.h
@@ -6,7 +6,6 @@
 #include<arpa/inet.h>
 #include<netdb.h>
 
-
 void die_with_user_message(const char *msg, const char *detail);
 void die_with_system_message(const char *msg);
 void handle_tcp_client(int clntSocket);
