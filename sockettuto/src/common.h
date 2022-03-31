e261a7e836de62670e3aaa5f12e0689be454b8d0
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-03-31 17:40:46 +0200

    Define print_socket_address() interface

--- sockettuto/src/common.h
+++ sockettuto/src/common.h
@@ -1,9 +1,15 @@
 #ifndef _SOCKECT_TUTORIAL_COMMON_H
 #define _SOCKECT_TUTORIAL_COMMON_H
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include <arpa/inet.h>
+#include<netdb.h>
+
 
 void die_with_user_message(const char *msg, const char *detail);
 void die_with_system_message(const char *msg);
 void handle_tcp_client(int clntSocket);
-
+void print_socket_address(const struct sockaddr *address, FILE *stream);
 
 #endif
