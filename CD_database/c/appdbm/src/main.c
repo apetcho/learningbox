3a74d8da809ff366f098ee6b8f4c7a1e3ed1fd5c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-04 18:25:54 +0200

    Implement main driver for application

--- CD_database/c/appdbm/src/main.c
+++ CD_database/c/appdbm/src/main.c
@@ -25,7 +25,7 @@ static int command_mode(int argc, char **argv);
 static void announce(void);
 static MenuOption show_menu(const CatalogEntry *current);
 static int get_confirm(const char *question);
-static int enter_new_catalog_entries(CatalogEntry *entry);
+static int enter_new_catalog_entry(CatalogEntry *entry);
 static void enter_new_track_entries(const CatalogEntry *entry);
 static void delete_catalog_entries(const CatalogEntry *entry);
 static void delete_track_entries(const CatalogEntry *entry);
@@ -40,7 +40,63 @@ static void strip_return(char *text);
 //     M A I N   D R I V E R
 // ----------------------------
 int main(int argc, char **argv){
+    MenuOption option;
+    CatalogEntry c_entry;
+    int cmdResult;
 
+    memset(&c_entry, '\0', sizeof(c_entry));
+
+    if(argc > 1){
+        cmdResult = command_mode(argc, argv);
+        exit(cmdResult);
+    }
+    announce();
+
+    if(!db_initialize(0)){
+        fprintf(stderr, "Sorry, unable to initialize database\n");
+        fprintf(stderr, "To create a new database use\n\t%s -i", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    while(option != MO_EXIT){
+        option = show_menu(&c_entry);
+        switch(option){
+        case MO_ADD_CAT:
+            if(enter_new_catalog_entry(&c_entry)){
+                if(!add_catalog_entry(c_entry)){
+                    fprintf(stderr, "Failed to add new entry\n");
+                    memset(&c_entry, '\0', sizeof(c_entry));
+                }
+            }
+            break;
+        case MO_ADD_TRACKS:
+            enter_new_track_entries(&c_entry);
+            break;
+        case MO_DEL_CAT:
+            delete_catalog_entries(&c_entry);
+            break;
+        case MO_FIND_CAT:
+            c_entry = find_catalog();
+            break;
+        case MO_LIST_CAT_TRACKS:
+            list_tracks(&c_entry);
+            break;
+        case MO_DEL_TRACKS:
+            delete_track_entries(&c_entry);
+            break;
+        case MO_COUNT_ENTRIES:
+            count_all_entries();
+            break;
+        case MO_EXIT:
+            break;
+        case MO_INVALID:
+            break;
+        default:
+            break;
+        }
+    }
+
+    db_close();
     return EXIT_SUCCESS;
 }
 
@@ -49,7 +105,7 @@ static int command_mode(int argc, char **argv){}
 static void announce(void){}
 static MenuOption show_menu(const CatalogEntry *current){}
 static int get_confirm(const char *question){}
-static int enter_new_catalog_entries(CatalogEntry *entry){}
+static int enter_new_catalog_entry(CatalogEntry *entry){}
 static void enter_new_track_entries(const CatalogEntry *entry){}
 static void delete_catalog_entries(const CatalogEntry *entry){}
 static void delete_track_entries(const CatalogEntry *entry){}
