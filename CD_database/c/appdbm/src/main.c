f3a41e04413ea0626d8e54446581a267b8154eb3
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-04 18:47:34 +0200

    Implement show_menu() for application

--- CD_database/c/appdbm/src/main.c
+++ CD_database/c/appdbm/src/main.c
@@ -111,8 +111,80 @@ static void announce(void){
     printf("%s", text);
 }
 
+// ---
+static MenuOption show_menu(const CatalogEntry *selected){
+    char tmpstr[TMP_STRING_LEN+1];
+    MenuOption chosen = MO_INVALID;
+
+    while(chosen == MO_INVALID){
+        if(selected->catalog[0]){
+            printf("\n\nCurrent entry: ");
+            printf("%s, %s, %s, %s, %s\n",
+                selected->catalog, selected->title,
+                selected->type, selected->artist
+            );
+            printf("\n");
+            puts("1 - Add new CD");
+            puts("2 - Search for a CD");
+            puts("3 - Count the CDs and tracks in the database");
+            puts("4 - Re-enter tracks for current CD");
+            puts("5 - Delete this CD, and all it's tracks");
+            puts("6 - List tracks for this CD");
+            puts("q - Quit\n");
+            printf("option>> ");
+            fgets(tmpstr, TMP_STRING_LEN, stdin);
+            switch(tmpstr[0]){
+            case '1':
+                chosen = MO_ADD_CAT;
+                break;
+            case '2':
+                chosen = MO_FIND_CAT;
+                break;
+            case '3':
+                chosen = MO_COUNT_ENTRIES;
+                break;
+            case '4':
+                chosen = MO_ADD_TRACKS;
+                break;
+            case '5':
+                chosen = MO_DEL_CAT;
+                break;
+            case '6':
+                chosen = MO_LIST_CAT_TRACKS;
+                break;
+            case 'q':
+                chosen = MO_EXIT;
+                break;
+            }
+        }else{
+            puts("\n");
+            puts("1 - Add new CD");
+            puts("2 - Search for a CD");
+            puts("3 - Count the CDs and tracks in the database");
+            puts("q - Quit\n");
+            printf("option>> ");
+            fgets(tmpstr, TMP_STRING_LEN, stdin);
+            switch(tmpstr[0]){
+            case '1':
+                chosen = MO_ADD_CAT;
+                break;
+            case '2':
+                chosen = MO_FIND_CAT;
+                break;
+            case '3':
+                chosen = MO_COUNT_ENTRIES;
+                break;
+            case 'q':
+                chosen = MO_EXIT;
+                break;
+            }
+        }
+    }
+
+    return chosen;
+}
+
 static int command_mode(int argc, char **argv){}
-static MenuOption show_menu(const CatalogEntry *current){}
 static int get_confirm(const char *question){}
 static int enter_new_catalog_entry(CatalogEntry *entry){}
 static void enter_new_track_entries(const CatalogEntry *entry){}
