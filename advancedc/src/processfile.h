b0cffcc041e06a0557725bce361bf1765f34208a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-27 17:06:21 +0200

    Update processfile.h skeleton

--- advancedc/src/processfile.h
+++ advancedc/src/processfile.h
@@ -3,19 +3,7 @@
 #include<stddef.h>
 #include<stdio.h>
 
-#define PERSONLEN       30
-#define MAXPERSON       1024
-#define PERSONBUFLEN    94
-
-typedef struct{
-    char *fname;
-    char *lname;
-    char *email;
-} Person_t;
-
-Person_t* person_alloc();
-void person_free(Person_t*);
-void person_print(Person_t);
+#include "person.h"
 
 int read_line(char *line, size_t len);
 Person_t parse_line(const char *line);
