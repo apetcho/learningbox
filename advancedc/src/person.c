5667168b6c6a464222ae0addc035fc6230e18184
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-27 17:22:26 +0200

    Implement person_alloc

---
+++ advancedc/src/person.c
@@ -0,0 +1,36 @@
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+#include "person.h"
+
+static void person_error(void *data, const char *routine){
+    if(data == NULL){
+        fprintf(
+            stderr, "%s:: %s : %d [%s]\n",
+            routine, __FILE__, __LINE__, strerror(errno)
+        );
+        exit(EXIT_FAILURE);
+    }
+    return;
+}
+
+/**
+ * @brief Allocate memory to Person object
+ * 
+ * @return Person_t* 
+ */
+Person_t* person_alloc(){
+    Person_t *person = (Person_t*)malloc(sizeof(Person_t));
+    person_error(person, "person_alloc()");
+    person->fname = (char*)malloc(sizeof(char)*PERSONLEN);
+    person_error(person->fname, "person_alloc()");
+    person->lname = (char*)malloc(sizeof(char)*PERSONLEN);
+    person_error(person->lname, "person_alloc()");
+    person->email = (char*)malloc(sizeof(char)*PERSONLEN);
+    person_error(person->email, "person_alloc()");
+    
+    return person;
+}
+
+void person_free(Person_t* person){}
+void person_print(Person_t person){}
