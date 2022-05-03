d1b83d4962bc57a4725ec7f473b33461d2f8fa74
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-03 20:56:17 +0200

    Explore producer/consumer queue with thread

--- ipctuto/src/ipcmicro.c
+++ ipctuto/src/ipcmicro.c
@@ -1,9 +1,10 @@
 
 #include<stdlib.h>
+#include<string.h>
 #include"ipcmicro.h"
 
-#define NUM_CHILDREN 8
-
+#define NUM_CHILDREN 2
+#define QUEUE_LENGTH 16
 
 //
 void ipcmicro_perror(char *message){
@@ -19,12 +20,88 @@ void* ipcmicro_malloc(size_t size){
     return data;
 }
 
-#ifdef SIMPLE_SHARED
+// ------ QUEUE DATA STRUCTURE --------
+QueueData* simple_new_queue(int len){
+    QueueData* queue = ipcmicro_malloc(sizeof(QueueData));
+    queue->len = len;
+    queue->array = (int*)ipcmicro_malloc((sizeof(QueueData)*len));
+    queue->nextIn = 0;
+    queue->nextOut = 0;
 
-SharedData* simple_new_shared_data(int n){
+    return queue;
+}
+
+int simple_queue_incr(QueueData *queue, int i){
+    return (i+1)% queue->len;
+}
+
+int simple_queue_empty(QueueData *queue){
+    return (queue->nextIn == queue->nextOut);
+}
+
+int simple_queue_full(QueueData *queue){
+    return (simple_queue_incr(queue, queue->nextIn) == queue->nextOut);
+}
+
+void simple_queue_push(QueueData *queue, int item){
+    if(simple_queue_full(queue)){
+        ipcmicro_perror("Queue is full");
+    }
+    queue->array[queue->nextIn] = item;
+    queue->nextIn = simple_queue_incr(queue, queue->nextIn);
+}
+
+int simple_queue_pop(QueueData *queue){
+    if(simple_queue_empty(queue)){
+        ipcmicro_perror("Queue is empty");
+    }
+    int item = queue->array[queue->nextOut];
+    queue->nextOut = simple_queue_incr(queue, queue->nextOut);
+    return item;
+}
+
+
+void simple_queue_test(){
+    char msg[] = "TESTING SIMPLE QUEUE DATA STRUCTURE";
+    size_t size = strlen(msg)+1+2;
+    char line[size];
+    memset(line, '-', sizeof(line));
+    printf("%s\n", line);
+    printf(" %s \n", msg);
+    printf("%s\n", line);
+    int item;
+    int len = 128;
+    QueueData *queue = simple_new_queue(len);
+    if(!simple_queue_empty(queue)){
+        ipcmicro_perror("Newly created queue is not empty");
+    }
+    for(int i=0; i < len; i++){
+        simple_queue_push(queue, i);
+    }
+    if(!simple_queue_full(queue)){
+        ipcmicro_perror("Fully loaded queue should be full");
+    }
+    for(int i=0; i < 10; i++){
+        item = simple_queue_pop(queue);
+        if(!(item==i)){
+            ipcmicro_perror("Weird order of item in queue");
+        }
+    }
+    if(simple_queue_empty(queue)){
+        ipcmicro_perror("Queue should not be empty at this moment");
+    }
+    if(simple_queue_full(queue)){
+        ipcmicro_perror("Queue should not be full at this moment");
+    }
+}
+
+
+// ------- END OF QUEUE DS -------
+
+
+SharedData* simple_new_shared_data(){
     SharedData *shared = ipcmicro_malloc(sizeof(SharedData));
-    shared->counter = 0;
-    (void)n;
+    shared->queue = simple_new_queue(QUEUE_LENGTH);
     return shared;
 }
 
@@ -35,7 +112,6 @@ pthread_t simple_new_thread(ThreadCallback_t fn, SharedData *shared){
     if(retval != 0){
         ipcmicro_perror("pthread_create failed");
     }
-
     return thread;
 }
 
@@ -46,96 +122,46 @@ void simple_join_thread(pthread_t thread){
     }
 }
 
-void* simple_callback(void *arg){
+void* simple_producer(void *arg){
     SharedData *shared = (SharedData*)arg;
-    simple_child(shared);
+    for(int i=0; i < QUEUE_LENGTH; i++){
+        printf("\x1b[32mProducing\x1b[m item \x1b[34m%d\x1b[m to queue\n", i);
+        simple_queue_push(shared->queue, i);
+    }
     pthread_exit(NULL);
 }
 
-void simple_child(SharedData *shared){
-    printf("counter = %d\n", shared->counter);
-    shared->counter++;
-}
-
-#endif
-
-#ifdef SIMPLE_MUTEX
-
-Mutex* simple_new_mutex(){
-    size_t size = sizeof(Mutex);
-    Mutex *mutex = (Mutex*)ipcmicro_malloc(size);
-    pthread_mutex_init(mutex->mutex, NULL);
-    return mutex;
-}
-
-void simple_lock(Mutex *mutex){
-    pthread_mutex_lock(mutex->mutex);
-}
-
-void simple_unlock(Mutex *mutex){
-    pthread_mutex_unlock(mutex->mutex);
-}
-
-SharedData* simple_new_shared_data(int n){
-    SharedData *shared = ipcmicro_malloc(sizeof(SharedData));
-    shared->counter = 0;
-    shared->mutex = simple_new_mutex();
-    return shared;
-}
-
-pthread_t simple_new_thread(ThreadCallback_t fn, SharedData *shared){
-    int retval;
-    pthread_t thread;
-    retval = pthread_create(&thread, NULL, fn, (void*)shared);
-    if(retval != 0){
-        ipcmicro_perror("pthread_create failed");
+void* simple_consumer(void *arg){
+    int item;
+    SharedData *shared = (SharedData*)arg;
+    for(int i=0; i < QUEUE_LENGTH; i++){
+        item = simple_queue_pop(shared->queue);
+        printf(
+            "\x1b[35mProducing\x1b[m item \x1b[34m%d\x1b[m from queue\n", item
+        );
     }
-    return thread;
-}
 
-void simple_join_thread(pthread_t thread){
-    int retval = pthread_join(thread, NULL);
-    if(retval == -1){
-        ipcmicro_perror("pthread_join failed");
-    }
-}
-
-void simple_child(SharedData *shared){
-    simple_lock(shared->mutex);
-    printf("counter = %d\n", shared->counter);
-    shared->counter++;
-    simple_unlock(shared->mutex);
-}
-
-void* simple_callback(void *arg){
-    SharedData* shared = (SharedData*)arg;
-    simple_child(shared);
     pthread_exit(NULL);
 }
 
-
-#endif
-
+#undef TEST_QUEUE
 // -----------------------------
 // -------- MAIN DRIVER --------
 // -----------------------------
 int main(int argc, char **argv){
+#define TEST_QUEUE
+#ifdef TEST_QUEUE
+    simple_queue_test();
+#else
+    pthread_t child[NUM_CHILDREN];
+    SharedData *shared = simple_new_shared_data();
+    child[0] = simple_new_thread(simple_producer, shared);
+    child[1] = simple_new_thread(simple_consumer, shared);
 
-#ifdef SIMPLE_SHARED
-    puts("\n\x1b[32mWorking in SIMPLE_SHARED mode\x1b[m\n");
-#elif defined(SIMPLE_MUTEX)
-    puts("\n\x1b[32mWorking in SIMPLE_MUTEX mode\x1b[m\n");
+    for(int i=0; i < NUM_CHILDREN; i++){
+        simple_join_thread(child[i]);
+    }
 #endif
-
-    pthread_t children[NUM_CHILDREN];
-    SharedData *shared = simple_new_shared_data(100000);
-    for(int i=0; i < NUM_CHILDREN; i++){
-        children[i] = simple_new_thread(simple_callback, shared);
-    }
-
-    for(int i=0; i < NUM_CHILDREN; i++){
-        simple_join_thread(children[i]);
-    }
 
     return 0;
 }
--- ipctuto/src/ipcmicro.h
+++ ipctuto/src/ipcmicro.h
@@ -3,77 +3,43 @@
 #include<pthread.h>
 #include<stdio.h>
 
-#undef SIMPLE_SHARED
-#define SIMPLE_MUTEX
-
-#ifdef SIMPLE_MUTEX
-    #define STRUCT_MUTEX
-    #if !defined(USE_MUTEX)
-    #   define USE_MUTEX
-    #endif
-#endif
-
-#ifdef SIMPLE_SHARED
-#if defined(USE_MUTEX)
-#undef USE_MUTEX
-#endif
-#endif
-
-#undef USE_MUTEX
-#ifdef SIMPLE_SHARED
-#define USE_MUTEX
-#endif
-
-
-void ipcmicro_perror(char *message);
-void* ipcmicro_malloc(size_t size);
-
-#ifdef USE_MUTEX
-#ifdef STRUCT_MUTEX
-    struct Mutex;
-    typedef struct Mutex Mutex;
-#else
-    typedef pthread_mutex_t Mutex;
-#endif
-
-Mutex* simple_new_mutex();
-void simple_lock(Mutex *mutex);
-void simple_unlock(Mutex *mutex);
-#endif
-
+typedef pthread_mutex_t Mutex;
+typedef pthread_cond_t Cond;
 typedef void* (ThreadCallback_t)(void *);
-struct SharedData;
-typedef struct SharedData SharedData;
 
-pthread_t simple_new_thread(ThreadCallback_t fn, SharedData *shared);
-void simple_join_thread(pthread_t thread);
+typedef struct QueueData{
+    int *array;
+    int len;
+    int nextIn;
+    int nextOut;
+    //Mutex *mutex;
+} QueueData;
 
-void simple_child(SharedData *shared);
-void* simple_callback(void *arg);
+typedef struct SharedData {
+    QueueData *queue;
+} SharedData;
 
-SharedData* simple_new_shared_data(int n);
-
-// -----------------------
-#ifdef SIMPLE_MUTEX
-typedef struct Mutex{
-    pthread_mutex_t mutex[1];
-} Mutex;
-
-struct SharedData{
-    int counter;
-    Mutex *mutex;
-};
-
-#endif // SIMPLE_MUTEX 
-
-// ----------------------------
-#ifdef SIMPLE_SHARED
-
-struct SharedData{
-    int counter;
-};
-#endif
+QueueData* simple_new_queue(int len);
+int simple_queue_incr(QueueData *queue, int i);
+int simple_queue_empty(QueueData *queue);
+int simple_queue_full(QueueData *queue);
+void simple_queue_push(QueueData *queue, int item);
+int simple_queue_pop(QueueData *queue);
+void* simple_producer(void *arg);
+void* simple_consumer(void *arg);
+void simple_queue_test();
 
 // 
+Cond* simple_new_cond();
+void simple_cond_wait(Cond *cond, Mutex *mutex);
+void simple_cond_signal(Cond *cond);
+
+SharedData* simple_new_shared_data();
+pthread_t simple_new_thread(ThreadCallback_t fn, SharedData *shared);
+void simple_join_thread(pthread_t thread);
+void simple_child(SharedData *shared);
+void* simple_callback(void *arg);
+void ipcmicro_perror(char *message);
+void* ipcmicro_malloc(size_t size);
 
 #endif // __IPCMICRO_H_
