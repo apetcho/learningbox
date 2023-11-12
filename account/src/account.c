154a85b4a682527d3e58e20538d0efd12c93dfec
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-12 16:32:50 +0100

    Implement menu driven inquiry c program

--- account/src/account.c
+++ account/src/account.c
@@ -1,23 +1,20 @@
+#include<stdbool.h>
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<signal.h>
+
+typedef enum {
+    ZERO_BALANCE = 1,
+    CREDIT_BALANCE,
+    DEBIT_BLANCE,
+    END
+} Option;
 
 // -*-
-void handle_interrupt(int sig);
-// -*-
-void prompt();
-void show(int account, const char* name, double balance);
-void readata(int *account, char *name, double *balance);
-// -*-
+bool showable(Option opt, double balance);
 
 // -*-------------------------*-
 // -*- M A I N   D R I V E R -*-
 // -*-------------------------*-
 int main(int argc, char **argv){
-    signal(SIGINT, handle_interrupt);
-    signal(SIGKILL, handle_interrupt);
-    signal(SIGTERM, handle_interrupt);
     FILE *fp = NULL;
     char *filename = NULL;
     if(argc > 1){
@@ -25,68 +22,70 @@ int main(int argc, char **argv){
     }else{
         filename = "account.txt";
     }
-
-    fp = fopen(filename, "r");
-    if(fp == NULL){
-        fprintf(stderr, "Error opening file '%s'\n", filename);
+    if((fp=fopen(filename, "r"))==NULL){
+        fprintf(stderr, "Unable to open file: '%s'\n", filename);
     }else{
-        // puts("Enter the account, name, and balance.");
-        // puts("Enter EOF to end input.");
-        // prompt();
-        int account = 0;
-        size_t len = 30;
-        char *name = calloc(len, sizeof(char));
-        double balance = 0.0;
-        memset(name, 0, len);
-        printf("%-10s%-13s%s\n", "Account", "Name", "Balance");
-        // readata(&account, name, &balance);
-        fscanf(fp, "%d%29s%lf", &account, name, &balance);
-        while(!feof(fp)){
-            // show(account, name, balance);
-            // fprintf(fp, "%d %s %.2lf\n", account, name, balance);
-            // prompt();
-            // memset(name, 0, len);
-            // readata(&account, name, &balance);
-            printf("%-10d%-13s%7.2lf\n", account, name, balance);
+        const char *menu = 
+            "Enter request\n"
+            " 1 - List accounts with zero balances\n"
+            " 2 - List accounts with credit balances\n"
+            " 3 - List accounts with debit balances\n"
+            " 4 - End of runt\n";
+        printf("%s", menu);
+        int request = 0;
+        scanf("%d", &request);
+
+        // - display records
+        while(request != END){
+            switch(request){
+            case ZERO_BALANCE:
+                puts("\nAccounts with zero balances:");
+                break;
+            case CREDIT_BALANCE:
+                puts("\nAccounts with credit balances:");
+                break;
+            case DEBIT_BLANCE:
+                puts("\nAccounts with debit balances:");
+                break;
+            }
+            // -
+            int account = 0;
+            char name[30] = "";
+            double balance = 0.0;
+
+            // read account, name and balance from file
             fscanf(fp, "%d%29s%lf", &account, name, &balance);
+            // read file contents (until eof)
+            while(!feof(fp)){
+                // output only if balance is 0
+                if(showable(request, balance)){
+                    printf("%-10d%-13s%7.2lf\n", account, name, balance);
+                }
+                // read account, name and balance from file
+                fscanf(fp, "%d%29s%lf", &account, name, &balance);
+            }
+            // return fp to beginning of file
+            rewind(fp);
+            printf("\n>> ");
+            scanf("%d", &request);
         }
-        free(name);
+        puts("End of run.");
         fclose(fp);
     }
-
+    
     return 0;
 }
 
 // -*-
-void prompt(){
-    printf(">> ");
-}
-
-// -*-
-void show(int account, const char* name, double balance){
-    printf("(%d, \"%s\", %.2lf)\n", account, name, balance);
-}
-
-// -*-
-void readata(int *account, char *name, double *balance){
-    char buffer[80];
-    memset(buffer, '\0', sizeof(buffer));
-    fgets(buffer, sizeof(buffer), stdin);
-    const char *sep = " ,";
-    char *next = NULL;
-    char *data = NULL;
-
-    data = strtok_r(buffer, sep, &next);
-    *account = atoi(data);
-    data = strtok_r(NULL, sep, &next);
-    strncpy(name, data, 30);
-    data = strtok_r(NULL, sep, &next);
-    *balance = strtod(data, NULL);
-}
-
-// -*-
-void handle_interrupt(int sig){
-    if(sig==SIGINT || sig==SIGKILL || sig==SIGTERM){
-        exit(EXIT_SUCCESS);
+bool showable(Option opt, double balance){
+    if((opt==ZERO_BALANCE) && (balance==0)){
+        return true;
     }
+    if((opt==CREDIT_BALANCE) && (balance < 0)){
+        return true;
+    }
+    if((opt==DEBIT_BLANCE) && (balance > 0)){
+        return true;
+    }
+    return false;
 }
